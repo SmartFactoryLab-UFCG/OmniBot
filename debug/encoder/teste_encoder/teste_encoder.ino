bb877dc9251062d5b43e1a1056c5b46cb9edda3b
Mateus Pincho <mateus.oliveira@ee.ufcg.edu.br>
2025-03-18 22:51:32 -0300

    modificacoes teste encoder

--- debug/encoder/teste_encoder/teste_encoder.ino
+++ debug/encoder/teste_encoder/teste_encoder.ino
@@ -30,20 +30,17 @@ class DCMotor {
    };
 
 
-DCMotor Motor1, Motor2, Motor3;
+DCMotor Motor1;
 
-// Yellow	encoder A output
-// White	encoder B output
 
-#define ENCODER_1_A 21
-#define ENCODER_1_B 20
-#define ENCODER_2_A 2
-#define ENCODER_2_B 3
-// #define ENCODER_3_A 18
-// #define ENCODER_3_B 19
+
+
+#define ENCODER_1_A 21 // Yellow	encoder A output
+#define ENCODER_1_B 20 // White	encoder B output
+
 
 volatile long totalPulses_1 = 0;
-// volatile long totalPulses_3 = 0;
+
 void countPulses_1(){
   if (digitalRead(ENCODER_1_B) > 0){
     totalPulses_1++;
@@ -54,25 +51,12 @@ void countPulses_1(){
 
 }
 
-volatile long totalPulses_2 = 0;
-// timeInitial = 0
-// tempo_atual = 0
-void countPulses_2(){ // get_time_between_pulses
-
-  
-  if (digitalRead(ENCODER_2_B) > 0){
-    totalPulses_2++; 
-  }
-  else{
-    totalPulses_2--;
-  }
-
   // tempo_atual = micros()
   // dt = tempo_atual - tempo_inicial
   // tempo_inicial = tempo_atual
 
   // dt_minutes = dt * 1e-6  * 0.01666
-}
+
 
 // Cálculo da velocidade rpm
 int interval = 1000; //  interval in ms
@@ -110,12 +94,12 @@ void loop() {
     previousMillis = currentMillis;
     
     // Revolutions per minute (RPM) = (total encoder pulse in 1s / encoder ppr) x 60s
-    rpm_2 = (float)(totalPulses_2 * 60 / countsPerRevolutation);
+    rpm_2 = (float)(totalPulses_1 * 60 / countsPerRevolutation);
 
     // Only update display when there have readings
     if ( rpm_2 > 0) 
     {
-      Serial.print(totalPulses_2);
+      Serial.print(totalPulses_1);
       Serial.print(" pulse / ");
       Serial.print(countsPerRevolutation);
       Serial.print(" pulse per rotation x 60 seconds = ");
@@ -124,17 +108,17 @@ void loop() {
     }
     else
     {
-      Serial.print(totalPulses_2);
+      Serial.print(totalPulses_1);
       Serial.print(" pulse / ");
       Serial.print(countsPerRevolutation);
       Serial.print(" pulse per rotation x 60 seconds = ");
       Serial.print(rpm_2);
       Serial.println(" RPM");
     }
-    totalPulses_2 = 0;
+    totalPulses_1 = 0;
   }
 
-  Motor1.Speed(200);
+  Motor1.Speed(255); // Máxima velocidade
   
   Motor1.Forward(); // Comando para o motor ir para frente
 
