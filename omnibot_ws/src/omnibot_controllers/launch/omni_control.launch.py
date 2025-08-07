8d8e30caede939e215e717297194462ecfb3ed06
Mateus Pincho de Oliveira <mateus.oliveira@ee.ufcg.edu.br>
2025-08-07 10:57:20 -0300

    add omni_kinematics node in the omni_control launch

--- omnibot_ws/src/omnibot_controllers/launch/omni_control.launch.py
+++ omnibot_ws/src/omnibot_controllers/launch/omni_control.launch.py
@@ -6,6 +6,23 @@ from launch.conditions import UnlessCondition, IfCondition
 
 
 def generate_launch_description():
+    # Declare args for the launch file
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+    )
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.03",
+    )
+    wheel_distance_arg = DeclareLaunchArgument(
+        "wheel_distance",
+        default_value="0.085",
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_distance = LaunchConfiguration("wheel_separation")
 
     # Spawn the controller manager for ros2 control
     joint_state_broadcaster_spawner = Node(
@@ -18,7 +35,7 @@ def generate_launch_description():
         ],
     )
 
-    # Spawn the wheel controller for ros2 control
+    # Spawn the wheel controller manager for ros2 control
     wheel_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -27,11 +44,24 @@ def generate_launch_description():
                    "/controller_manager"
                 ]
     )
+
+    # Spawn the kinematics controller
+    omni_kinematics = Node(
+        package='omnibot_controllers',
+        executable='omni_kinematics',
+        parameters=[
+            {'wheel_radius':wheel_radius,
+             'wheel_distance':wheel_distance,
+             'use_sim_time':use_sim_time}]
+    )
   
     return LaunchDescription(
         [
             joint_state_broadcaster_spawner,
             wheel_controller_spawner,
-
+            wheel_radius_arg,
+            wheel_distance_arg,
+            use_sim_time_arg,
+            omni_kinematics
         ]
     )
